2dcd4819b0565d84dbbe5494ce77db46442ac967
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-05-16 11:49:21 +0530

    Auusie/Balaji | #0 | added tomcat6 module to scm

---
+++ puppet/modules/tomcat6/manifests/init.pp
@@ -0,0 +1,38 @@
+class tomcat {
+  include users
+  
+  exec {"gettomcattarfile" :
+  	command => "/usr/bin/wget -O /tmp/apache-tomcat-6.0.35.tar.gz http://motechrepo.github.com/pub/motech/other/apache-tomcat-6.0.35.tar.gz",
+  	require => [Exec["${motechUser} homedir"]]
+  }
+  
+  exec { "tomcat_untar":
+    command => "tar xfz /tmp/apache-tomcat-6.0.35.tar.gz",
+    user => "${motechUser}",
+    cwd     => "/home/${motechUser}",
+    creates => "/home/${motechUser}/apache-tomcat-6.0.35",
+    path    => ["/bin"],
+    require => Exec["gettomcattarfile"],
+  }
+
+  file { "/etc/init.d/tomcat" :
+  	content => template("tomcat/tomcat.initd"),
+  	mode   =>  777,
+  	group  => "root",
+  	owner  => "root",
+  	require => Exec["tomcat_untar"],
+  }
+
+  exec { "installtomcatservice" :
+  	command => "/sbin/chkconfig --add tomcat",
+  	user => "root", 
+  	require => File["/etc/init.d/tomcat"],
+  }
+  
+  service { "tomcat":
+  	ensure => running,
+  	enable => true,
+  	hasstatus => false,
+  	require => Exec["installtomcatservice"],
+  }
+}
