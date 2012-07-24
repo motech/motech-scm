642287b98312fce7f3f45edf1b57066c0c50d7a9
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-07-24 16:15:43 +0000

    balaji | changing zip to tar file for birt module

--- puppet/modules/birt/manifests/init.pp
+++ puppet/modules/birt/manifests/init.pp
@@ -2,12 +2,12 @@ class birt {
   include users
   
   exec {"getbirttarfile" :
-  	command => "/usr/bin/wget -O /tmp/birt-runtime-2_5_2.zip http://motechrepo.github.com/pub/motech/other/birt-runtime-2_5_2.zip",
+  	command => "/usr/bin/wget -O /tmp/birt-runtime-2_5_2.tar.gz http://motechrepo.github.com/pub/motech/other/birt-runtime-2_5_2.tar.gz",
   	require => [Exec["${motechUser} homedir"]]
   }
   
   exec { "birt_untar":
-    command => "unzip /tmp/birt-runtime-2_5_2.zip",
+    command => "tar xfz /tmp/birt-runtime-2_5_2.tar.gz",
     user => "${motechUser}",
     cwd     => "/home/${motechUser}/",
     creates => "/home/${motechUser}/",
