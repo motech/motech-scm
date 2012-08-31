a5423872071ebfccfa722be50837741785c40f8c
Sharvani <sharvani.n.s@gmail.com>
2012-08-31 10:52:17 +0530

    sharvani,sush | #2055 | Modified config for addition of public keys.

--- puppet/modules/ssh/templates/configure_public_keys.sh
+++ puppet/modules/ssh/templates/configure_public_keys.sh
@@ -2,10 +2,7 @@ publicKeyFilePath=$1
 motechUser=$2
 
 #add public keys to authorized keys if does not exist
-for key in `cat $publicKeyFilePath`
-do
-    if [[ -z `grep $key /home/$motechUser/.ssh/authorized_keys` ]]
-    then
-        cat $key >> /home/$motechUser/.ssh/authorized_keys
-    fi
-done
+if [[ -z `grep -f $publicKeyFilePath /home/$motechUser/.ssh/authorized_keys` ]]
+then
+    cat $publicKeyFilePath >> /home/$motechUser/.ssh/authorized_keys
+fi
