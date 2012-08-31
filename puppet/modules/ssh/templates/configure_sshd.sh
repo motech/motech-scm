83533a4c6cb1893c5b87ed13206451ae0b92dfca
Sharvani <sharvani.n.s@gmail.com>
2012-08-31 17:38:29 +0530

    Ashish,Sharvani | #00 | Modifying script for configuring ssh.

--- puppet/modules/ssh/templates/configure_sshd.sh
+++ puppet/modules/ssh/templates/configure_sshd.sh
@@ -1,15 +1,15 @@
 port=$1
 motechUser=$2
 DeactivatePasswordAuthentication=$3
-configFile=/etc/ssh/sshd_config
+configFile=/home/motech/temp/sshd_config
 
 findAndReplace()
 {
-    if [[ -z `grep ^$1 $configFile` ]]
+    if [[ -z `grep "^\s*$1\>" $configFile` ]]
     then
         echo $1 $2 >> $configFile
     else
-        sed -i "s/^$1\\t* .*/$1 $2/" $configFile
+        sed -i "s/^\s*$1\>.*/$1 $2/" $configFile
     fi
 }
 
@@ -18,7 +18,7 @@ findAndReplace "PermitRootLogin" "no"
 findAndReplace "X11Forwarding" "no"
 findAndReplace "AllowUsers" "$motechUser"
 
-if $DeactivatePasswordAuthentication
+if [ "$DeactivatePasswordAuthentication" == "true" ]
 then
     findAndReplace "PasswordAuthentication" "no"
 fi
