d83d35dc07399898787d43f66e6437a2124400b3
sneha koti <snehakoti@gmail.com>
2012-09-03 15:01:55 +0530

    Imdad, Sneha | #170 | Improving bootstrap shell script for vagrant vm

--- vagrant-vm/bootstrap.sh
+++ vagrant-vm/bootstrap.sh
@@ -1,11 +1,21 @@
 boxImageLocation=$1
 configurationFileLocation=$2
 
-cd /tmp/ && git clone git://github.com/motech/motech-scm.git -b incremental-deployment && \
-cd ./motech-scm && git checkout incremental-deployment && \
-cp $2 ./puppet/manifests/nodes/configuration.pp && \
-cp ./puppet/manifests/site.pp ./puppet/manifests/default.pp && \
-cp ./vagrant-vm/Vagrantfile ./puppet/ && \
-cd ./puppet/ && \
-vagrant box add centos6 $boxImageLocation && \
-vagrant up
+centos6BoxExists=`vagrant box list | grep centos6 | wc -l`
+
+if [ ! -d "/tmp/motech-scm" ]
+then
+  cd /tmp/ && git clone git://github.com/motech/motech-scm.git -b incremental-deployment
+fi
+
+cd /tmp/motech-scm
+cp $configurationFileLocation ./puppet/manifests/nodes/configuration.pp
+cp ./vagrant-vm/Vagrantfile ./puppet/
+cd ./puppet/
+
+if [ $centos6BoxExists -eq 0 ]
+then
+  vagrant box add centos6 $boxImageLocation && vagrant up
+else
+  vagrant destroy -f && vagrant up
+fi
