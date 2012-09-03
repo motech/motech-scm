e6574bf7f29b677964f959e07ae59af7313fc38e
sneha koti <snehakoti@gmail.com>
2012-09-03 16:02:10 +0530

    Imdad,Sneha| #170| Modified script to expect user to give the location of motech-scm directory

--- vagrant-vm/bootstrap.sh
+++ vagrant-vm/bootstrap.sh
@@ -1,13 +1,21 @@
 boxImageLocation=$1
 configurationFileLocation=$2
+motechSCMLocation=$3
 
 centos6BoxExists=`vagrant box list | grep centos6 | wc -l`
+wd=`pwd`
 
-if [ ! -d "/tmp/motech-scm" ]
+if [ -d "/tmp/motech-scm" ]
 then
-  cd /tmp/ && git clone git://github.com/motech/motech-scm.git -b incremental-deployment
+  if [ -f "/tmp/motech-scm/puppet/.vagrant" ]
+  then
+    cd /tmp/motech-scm/puppet/ && vagrant destroy -f
+  fi
+  rm -rf /tmp/motech-scm
 fi
 
+cd $wd
+cp -r $motechSCMLocation /tmp/
 cd /tmp/motech-scm
 cp $configurationFileLocation ./puppet/manifests/nodes/configuration.pp
 cp ./vagrant-vm/Vagrantfile ./puppet/
@@ -17,5 +25,5 @@ if [ $centos6BoxExists -eq 0 ]
 then
   vagrant box add centos6 $boxImageLocation && vagrant up
 else
-  vagrant destroy -f && vagrant up
+  vagrant up
 fi
