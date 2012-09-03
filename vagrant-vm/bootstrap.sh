95d2e3b4f9ed89c1f101c7672cce2a7b6805ce36
sneha koti <snehakoti@gmail.com>
2012-09-03 12:59:26 +0530

    imdad,sneha | #170 | Fixing error

--- vagrant-vm/bootstrap.sh
+++ vagrant-vm/bootstrap.sh
@@ -4,7 +4,7 @@ configurationFileLocation=$2
 cd /tmp/ && git clone git://github.com/motech/motech-scm.git -b incremental-deployment && \
 cd ./motech-scm && git checkout incremental-deployment && \
 cp $2 ./puppet/manifests/nodes/configuration.pp && \
-cp ./puppet/manifests/site.pp ./motech-scm/puppet/manifests/default.pp && \
+cp ./puppet/manifests/site.pp ./puppet/manifests/default.pp && \
 cp ./vagrant-vm/Vagrantfile ./puppet/ && \
 cd ./puppet/ && \
 vagrant box add centos6 $boxImageLocation && \
