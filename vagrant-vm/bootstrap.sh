c960b2cb738f276801ee9d48a2b906f8b07f781c
sneha koti <snehakoti@gmail.com>
2012-09-03 12:49:36 +0530

    Imdad,Sneha | #00 | Updated bootstrap.sh

--- vagrant-vm/bootstrap.sh
+++ vagrant-vm/bootstrap.sh
@@ -1,10 +1,11 @@
 boxImageLocation=$1
 configurationFileLocation=$2
 
-cd /tmp && \
-cd /tmp/ && git clone git://github.com/motech/motech-scm.git -b incremental-development && \
-cp $2 ./motech-scm/puppet/manifests/nodes && \
-cp ./motech-scm/puppet/manifests/site.pp ./motech-scm/puppet/manifests/default.pp && \
-cd ./motech-scm/puppet/ && \
+cd /tmp/ && git clone git://github.com/motech/motech-scm.git -b incremental-deployment && \
+cd ./motech-scm && git checkout incremental-deployment && \
+cp $2 ./puppet/manifests/nodes/configuration.pp && \
+cp ./puppet/manifests/site.pp ./motech-scm/puppet/manifests/default.pp && \
+cp ./vagrant-vm/Vagrantfile ./puppet/ && \
+cd ./puppet/ && \
 vagrant box add centos6 $boxImageLocation && \
 vagrant up
