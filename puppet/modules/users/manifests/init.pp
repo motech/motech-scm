d41ae8b5acc4b4d3516973b83dd8d10b81c4a609
Srivatsa Katta <vatsa.katta@gmail.com>
2012-09-07 23:55:18 +0530

    Katta| Creating motech user only if does not exists (idempotent) now

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -1,29 +1,34 @@
-
 class users ( $userName, $password ) {
-  user { "${userName}":
-      ensure     => present,
-      shell      => "/bin/bash",
-      home       => "/home/${userName}",
-      password   => $password,
-  }
-  
-  exec { "$userName homedir":
-    command => "/bin/cp -R /etc/skel /home/$userName; /bin/chown -R $userName:$userName /home/$userName",
-    creates => "/home/$userName",
-    require => User[$userName],
-  }
 
-  file { "add-user-to-sudoers.sh" :
-    path => "/home/${userName}/add-user-to-sudoers.sh",
-    content => template("users/add-user-to-sudoers.sh"),
-    owner => "${userName}",
-    group => "${userName}",
-    mode   =>  764,
-    require => Exec["$userName homedir"],
-  }
+    user { "${userName}":
+        ensure      => present,
+        shell       => "/bin/bash",
+        home        => "/home/${userName}",
+        password    => $password,
+    }
 
-  exec { "add-user-to-sudoers":
-      command => "/bin/sh /home/${userName}/add-user-to-sudoers.sh ${userName}",
-      require => File["add-user-to-sudoers.sh"],
-  }
+    exec { "$userName homedir":
+        provider    => "shell",
+        command     => "/bin/cp -R /etc/skel /home/$userName; /bin/chown -R $userName:$userName /home/$userName",
+        creates     => "/home/$userName",
+        onlyif      => "test ! -f /home/${userName}/.bashrc",
+        require     => User[$userName],
+    }
+
+    file { "add-user-to-sudoers.sh" :
+        ensure      => present,
+        path        => "/home/${userName}/add-user-to-sudoers.sh",
+        content     => template("users/add-user-to-sudoers.sh"),
+        owner       => "${userName}",
+        group       => "${userName}",
+        mode        => 764,
+        require     => Exec["$userName homedir"],
+    }
+
+    exec { "add-user-to-sudoers":
+        provider    => "shell",
+        command     => "/bin/sh /home/${userName}/add-user-to-sudoers.sh ${userName}",
+        onlyif      => "test `grep -i ${userName} /etc/sudoers  | wc -l` -eq 0",
+        require     => File["add-user-to-sudoers.sh"],
+    }
 }
