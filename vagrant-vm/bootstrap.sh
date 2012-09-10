3fd39bc497d08298b8b82c4b640ddb7191cb7ff1
f1codz <imdad.ahmed@gmail.com>
2012-09-10 17:13:25 +0530

    Imdad | #00 | Caching location of application configuration.pp so that changes to it may not require manual copy.

--- vagrant-vm/bootstrap.sh
+++ vagrant-vm/bootstrap.sh
@@ -6,6 +6,7 @@ myDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 commonPP="$myDir/common.pp"
 modulesDir="$myDir/../puppet/modules"
 manifestsDir="$myDir"
+vmPropertiesFile="$myDir/.testvm.properties"
 # Globals End
 
 # Functions Start
@@ -16,6 +17,25 @@ setup_vagrantfile(){
   mv Vagrantfile{.bk,}
 }
 
+load_config(){
+  local configFile=$1
+
+  cp $commonPP config.pp && echo "created config.pp from common.pp"
+  cat $configFile >> config.pp && echo "application config added to config.pp"
+}
+
+reload_config(){
+  if [ -f $vmPropertiesFile ]; then
+    local configFile=`cat $vmPropertiesFile | grep configFile | cut -d":" -f2`
+    [[ -f $configFile ]] && load_config $configFile && echo "reloaded configFile{:$configFile}"
+  fi
+}
+
+save_config_file_path(){
+  local configFile=$1
+  echo "configFile:$configFile" > $vmPropertiesFile && echo "saved path to configFile{:$configFile}"
+}
+
 init(){
   clear
 
@@ -24,27 +44,28 @@ init(){
   local boxFileName=${boxFile##*/}
 
   cd $myDir
-  vagrant box add $boxFileName $boxFile && echo "VM added"
-  vagrant init $boxFileName && echo "Vagrantfile created"
+  command vagrant box add $boxFileName $boxFile && echo "VM added"
+  command vagrant init $boxFileName && echo "Vagrantfile created"
   setup_vagrantfile && echo "Vagrantfile setup done"
 
-  cp $commonPP config.pp && echo "created config.pp from common.pp"
-  cat $configFile >> config.pp && echo "application config added to config.pp"
+  save_config_file_path $configFile
+  load_config $configFile
 }
 
 clear(){
   if [ -f Vagrantfile ]; then
     local boxFileName=`grep "  config.vm.box" Vagrantfile | sed 's/.*\"\(.*\)\"/\1/g'`
-    local boxExists=`vagrant box list | grep $boxFileName | wc -l`
-    local boxDestroyed=`vagrant status | grep default | grep "not created" | wc -l`
+    local boxExists=`command vagrant box list | grep $boxFileName | wc -l`
+    local boxDestroyed=`command vagrant status | grep default | grep "not created" | wc -l`
 
-    [[ boxDestroyed -eq 0 ]] && vagrant destroy -f && echo "VM destroyed"
-    [[ ! boxExists -eq 0 ]] && vagrant box remove $boxFileName && echo "VM removed"
-
+    [[ boxDestroyed -eq 0 ]] && command vagrant destroy -f && echo "VM destroyed"
+    [[ ! boxExists -eq 0 ]] && command vagrant box remove $boxFileName && echo "VM removed"
+    
     rm Vagrantfile && echo "Vagrantfile removed"
   fi
 
   [[ -f config.pp ]] && rm -f config.pp && echo "config.pp removed"
+  [[ -f $vmPropertiesFile ]] && rm -f $vmPropertiesFile && echo ".testvm.properties deleted"
 }
 
 usage(){
@@ -53,6 +74,13 @@ usage(){
   echo "2) Run: <sh bootstap.sh init /path/to/image.box /path/to/configuration.pp> This creates Vagrantfile and initiates vagrant"
   echo "3) From with-in vagrant-vm execute vagrant up/halt/reload/package (http://tinyurl.com/d4ljaxm) to test your changes in motech-scm/puppet/modules or configuration.pp or Vagrantfile"
   echo "4) Run: <sh bootstrap.sh clear> to delete extra created files and destroy vagrant"
+}
+
+vagrant(){
+  if [ ! -z `echo "up reload" | grep -o $1` ]; then
+    reload_config
+  fi
+  command vagrant $@
 }
 # Functions End
 
