0c18a9a86db28eabbd91b04eaed8221d5fc64fe9
Sharvani <sharvani.n.s@gmail.com>
2012-09-11 10:53:41 +0530

    Sharvani | #2058 | adding permissions to nagios plugin files when copied.

--- puppet/modules/nagios/manifests/init.pp
+++ puppet/modules/nagios/manifests/init.pp
@@ -48,6 +48,7 @@
     file { "/usr/lib64/nagios/plugins/":
       source    => "/tmp/nagios_package/${nagios_plugins_path}",
       recurse   => true,
+      mode      =>  554,
       require   => File["/etc/nagios/objects/"]
     }
 
