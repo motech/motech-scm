a8d59aeb87070e433dc0c9fda3f7b2b270522b5c
f1codz <imdad.ahmed@gmail.com>
2012-09-13 14:19:45 +0530

    Imdad, Ashok | #2064 | Modified httpd class which now only ensures httpd installed and running. As of now, Project specific rules need to be inserted manually into httpd.conf and ssl.conf

--- puppet/modules/httpd/manifests/init.pp
+++ puppet/modules/httpd/manifests/init.pp
@@ -1,78 +1,88 @@
+# This class as of now ensures httpd installed and running
+# Project specific rules need to be inserted manually into httpd.conf and ssl.conf
 
-class httpd ( $userName, $httpdMachine, $httpdProxyPort, $httpdMasterHost, $httpdMasterPort, $httpdSlaveHost, $httpdSlavePort, $httpToHttpsRedirectionEnabled, $httpInternalPortRedirectionEnabled, $httpsExcludedHostAddress, $apacheHttpPort, $httpSslPort, $apacheTomcatPort) {
+class httpd () { #($config_url, $config_path) {
     package { "httpd" :
-        ensure => "present",
+        ensure => "present"
     }
-
-    if "${httpdMachine}" == 'failoverProxy' {
-        file {"/home/${httpd::userName}/config-httpd-load-balance.sh" :
-            content => template("httpd/config-httpd-load-balance.sh"),
-            owner => "${httpd::userName}",
-            group => "${httpd::userName}",
-            mode   =>  764,
-        }
-
-        exec { "config-httpd-load-balance":
-            require => File["/home/${httpd::userName}/config-httpd-load-balance.sh"],
-            command => "/bin/sh /home/${httpd::userName}/config-httpd-load-balance.sh ${httpd::httpdProxyPort} ${httpd::httpdMasterHost} ${httpd::httpdMasterPort} ${httpd::httpdSlaveHost} ${httpd::httpdSlavePort}",
-            notify => Service["httpd"],
-        }
-    }
-
-    # class { "httpd::failoverProxy" :
-    #     httpdMachine => "${httpdMachine}",
-    #     require => Package["httpd"],
-        
-    # }
-
-    if "${httpToHttpsRedirectionEnabled}" == 'true' {
-        file {"/home/${httpd::userName}/config-httpd-redirect-internal-to-port.sh" :
-            content => template("httpd/config-httpd-redirect-internal-to-port.sh"),
-            owner => "${httpd::userName}",
-            group => "${httpd::userName}",
-            mode   =>  764,
-        }
-
-        exec { "config-httpd-redirect-internal-to-port-for-httpd-conf":
-            require => File["/home/${httpd::userName}/config-httpd-redirect-internal-to-port.sh"],
-            command => "/bin/sh /home/${httpd::userName}/config-httpd-redirect-internal-to-port.sh ${httpd::apacheHttpPort} ${httpd::apacheTomcatPort} /etc/httpd/conf/httpd.conf"
-        }
-
-        exec { "config-httpd-redirect-internal-to-port-for-ssl-conf":
-            require => File["/home/${httpd::userName}/config-httpd-redirect-internal-to-port.sh"],
-            command => "/bin/sh /home/${httpd::userName}/config-httpd-redirect-internal-to-port.sh ${httpd::httpSslPort} ${httpd::apacheTomcatPort} /etc/httpd/conf.d/ssl.conf"
-        }
-    }
-
-    # class { "httpd::httpToHttpsRedirection" :
-    #     enabled => "${httpToHttpsRedirectionEnabled}",
-    #     require => Package["httpd"],
-    #     notify => Service["httpd"],
-    # }
-
-    if "${httpInternalPortRedirectionEnabled}" == 'true' {
-        file {"/home/${httpd::userName}/config-httpd-redirect-to-https.sh" :
-            content => template("httpd/config-httpd-redirect-to-https.sh"),
-            owner => "${httpd::userName}",
-            group => "${httpd::userName}",
-            mode   =>  764,
-        }
-
-        exec { "config-httpd-redirect-to-https":
-            require => File["/home/${httpd::userName}/config-httpd-redirect-to-https.sh"],
-            command => "/bin/sh /home/${httpd::userName}/config-httpd-redirect-to-https.sh ${httpd::httpsExcludedHostAddress}"
-        }
-    }
-
-    # class { "httpd::httpInternalPortRedirection" :
-    #     enabled => "${httpInternalPortRedirectionEnabled}",
-    #     require => Package["httpd"],
-    #     notify => Service["httpd"],
-    # }
 
     service {"httpd" :
         ensure => "running",
         enable => true,
-        require => Package["httpd"],
+        require => Package["httpd"]
     }
+
+    # file { "/tmp/httpd_package" :
+    #     purge  => true,
+    #     recurse => true,
+    #     force => true,
+    #     ensure  => "directory",
+    #     require => Package["httpd"]
+    # }
+
+    # exec { "wget '${config_url}' -O config.jar" :
+    #     alias   => "fetch_httpd_config_package",
+    #     cwd     => "/tmp/httpd_package",
+    #     require => File["/tmp/httpd_package"]
+    # }
+
+    # exec { "jar -xvf config.jar" :
+    #     cwd     => "/tmp/httpd_package",
+    #     alias   => "unjar_httpd_package",
+    #     require => Exec["fetch_httpd_config_package"]
+    # }
+
+    # file { "/tmp/apply_conf_part.sh" :
+    #     source => "puppet:///modules/httpd/apply_conf_part.sh"
+    # }
+
+    # file { "/tmp/httpd.conf.part" :
+    #     source => "puppet:///modules/httpd/httpd.conf.part"
+    # }
+
+    # file { "/tmp/ssl.conf.part" :
+    #     source => "puppet:///modules/httpd/ssl.conf.part"
+    # }
+    
+    ###### httpd.conf ######
+    ## take a backup
+    # file { "/etc/httpd/conf/httpd.conf.bk" :
+    #     source => "/etc/httpd/conf/httpd.conf",
+    #     require => Package["httpd"]
+    # }
+
+    ## create a httpd.conf from default
+    # file { "/etc/httpd/conf/httpd.conf" :
+    #     source => "puppet:///modules/httpd/httpd.conf.default",
+    #     require => File["/etc/httpd/conf/httpd.conf.bk"],
+    #     notify => Service["httpd"]
+    # }
+
+    ## apply httpd.conf.part
+    # exec { "sh /tmp/apply_conf_part.sh /tmp/httpd.conf.part /etc/httpd/conf/httpd.conf" :
+    #     require => File["/tmp/httpd.conf.part", "/etc/httpd/conf/httpd.conf", "/etc/httpd/conf/httpd.conf.bk", "/tmp/apply_conf_part.sh"],
+    #     alias => "apply_httpd_conf_part",
+    #     notify => Service["httpd"]
+    # }
+
+    ###### ssl.conf ######
+    ## take a backup
+    # file { "/etc/httpd/conf.d/ssl.conf.bk" :
+    #     source => "/etc/httpd/conf.d/ssl.conf",
+    #     require => Package["httpd"]
+    # }
+
+    ## create a ssl.conf from default
+    # file { "/etc/httpd/conf.d/ssl.conf" :
+    #     source => "puppet:///modules/httpd/ssl.conf.default",
+    #     require => File["/etc/httpd/conf.d/ssl.conf.bk"],
+    #     notify => Service["httpd"]
+    # }
+
+    ## apply ssl.conf.part
+    # exec { "sh /tmp/apply_conf_part.sh /tmp/ssl.conf.part /etc/httpd/conf.d/ssl.conf" :
+    #     require => File["/tmp/ssl.conf.part", "/etc/httpd/conf.d/ssl.conf", "/etc/httpd/conf.d/ssl.conf.bk", "/tmp/apply_conf_part.sh"],
+    #     alias => "apply_ssl_conf_part",
+    #     notify => Service["httpd"]
+    # }
 }
