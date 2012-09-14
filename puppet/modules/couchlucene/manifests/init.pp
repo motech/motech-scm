91ca11f1aa99ba0b8a26f875d111d24d886c4c2e
Vishal Naik <vishalnaik@thoughtworks.com>
2012-09-14 12:11:45 +0530

    Vishal,Vishnu #0 Updated couchdb-lucene-init.d

--- puppet/modules/couchlucene/manifests/init.pp
+++ puppet/modules/couchlucene/manifests/init.pp
@@ -1,62 +1,45 @@
-class couchlucene ($version) {
+class couchdblucene ($version) {
 
-    exec { "get_couch_lucene_tar" :
-        command     => "/usr/bin/wget -O /tmp/couch-lucene.tar.gz http://dl.dropbox.com/u/102967387/couchdb-lucene-0.9.0-SNAPSHOT-dist.tar.gz",
+ =begin
+    exec { "get_couchdb_lucene_tar" :
+        command     => "/usr/bin/wget -O /tmp/couchdb-lucene.tar.gz http://dl.dropbox.com/u/102967387/couchdb-lucene-0.9.0-SNAPSHOT-dist.tar.gz",
         timeout     => 0,
         provider    => "shell",
-        onlyif      => "test ! -f /tmp/couch-lucene.tar.gz"
+        onlyif      => "test ! -f /tmp/couchdb-lucene.tar.gz"
     }
+=end
 
-
-    exec { "couch_lucene_untar" :
-        command     => "tar xfz /tmp/couch-lucene.tar.gz",
+    exec { "couchdb_lucene_untar" :
+        command     => "tar xfz /tmp/couchdb-lucene.tar.gz",
         user        => "${motechUser}",
         cwd         => "/home/${motechUser}",
-        creates     => "/home/${motechUser}/couch-lucene",
+        creates     => "/home/${motechUser}/couchdb-lucene",
         path        => ["/bin",],
-        require     => [Exec["${motechUser} homedir"], Exec["get_couch_lucene_tar"]],
+        require     => [Exec["${motechUser} homedir"]],
         provider    => "shell",
-        onlyif      => "test ! -d /home/${motechUser}/couch-lucene"
+        onlyif      => "test ! -d /home/${motechUser}/couchdb-lucene"
     }
 
-    file { "/etc/init.d/couch_lucene" :
+    file { "/etc/init.d/couchdb_lucene" :
         ensure      => present,
-        content     => template("couchlucene/couchlucene-init.d"),
+        content     => template("couchdblucene/couchdb-lucene-init.d"),
         mode        =>  777,
         group       => "root",
         owner       => "root",
-        require     => Exec["couch_lucene_untar"],
+        require     => Exec["couchdb_lucene_untar"],
     }
 
-    define replace($file, $pattern, $replacement) {
-    	$pattern_no_slashes = slash_escape($pattern)
-    	$replacement_no_slashes = slash_escape($replacement)
-
-    	exec { "/usr/bin/perl -pi -e 's/$pattern_no_slashes/$replacement_no_slashes/' '$file'":
-    	    onlyif => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /$pattern_no_slashes/ && ! /$replacement_no_slashes/ ; END { exit \$ret; }' '$file'",
-    	}
-    }
-
-    replace { "append_http_global_handlers":
-	file => "/etc/couchdb/local.ini",
-        pattern => "[httpd_global_handlers]",
-	replacement => "[httpd_global_handlers]\n _fti = {couch_httpd_proxy, handle_proxy_req, <<\"http://127.0.0.1:5985\">>}",
-	before => Service["couch_lucene"],
-	require => Package["couchdb"],
-	notify => Service["couchdb"],
-    }
-
-    exec { "install_couch_lucene_service" :
-        command     => "/sbin/chkconfig --add couch_lucene",
+    exec { "install_couchdb_lucene_service" :
+        command     => "/sbin/chkconfig --add couchdb-lucene",
         user        => "root",
-        require     => File["/etc/init.d/couch_lucene"],
-        onlyif      => "chkconfig --list couch_lucene; [ $? -eq 1 ]"
+        require     => File["/etc/init.d/couchdb-lucene"],
+        onlyif      => "chkconfig --list couchdb-lucene; [ $? -eq 1 ]"
     }
 
-    service { "couch_lucene":
+    service { "couchdb_lucene":
         ensure     => running,
-        path       => "/home/${motechUser}/couch-lucene/bin/run",
+        path       => "/home/${motechUser}/couchdb-lucene/bin/run",
         enable     => true,
-        require    => Exec["install_couch_lucene_service"],
+        require    => Exec["install_couchdb_lucene_service"],
     }
 }
--- puppet/modules/couchlucene/templates/couchlucene-init.d
+++
@@ -1,9 +0,0 @@
-case "$1" in
-
-  start)
-    /home/${motechUser}/couch-lucene/bin/run
-    ;;
-  stop)
-    ps aux | grep couch-lucene | cut 3-3 | kill -9
-    ;;
-esac
