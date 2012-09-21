989ae280ba3f1907e0f4647e1a140c95dfd33e20
Srivatsa Katta <vatsa.katta@gmail.com>
2012-09-21 23:09:57 +0530

    Katta | Merged ssl manifest with httpd module and using templates to configure ssl

--- puppet/modules/httpd/files/apply_conf_part.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-part_file=$1
-conf_file=$2
-
-f2="$(<$part_file)"
-awk -vf2="$f2" '/## Custom changes ##/{print f2;print;next}1' $conf_file > /tmp/full.conf.bk
-mv /tmp/full.conf.bk $conf_file
--- puppet/modules/httpd/manifests/init.pp
+++ puppet/modules/httpd/manifests/init.pp
@@ -1,7 +1,7 @@
 # This class as of now ensures httpd installed and running
 # Project specific rules need to be inserted manually into httpd.conf and ssl.conf
 
-class httpd () { #($config_url, $config_path) {
+class httpd ($sslEnabled, $sslCertificateFile, $sslCertificateKeyFile) {
 
     package { "httpd" :
         ensure => "present"
@@ -25,78 +25,22 @@ class httpd () { #($config_url, $config_path) {
         require     => Package["httpd"]
     }
 
+	if $sslEnabled == 'true' {
+		package { "mod_ssl" :
+		    ensure      =>  "present",
+		    require     => File["/etc/httpd/conf/httpd.conf"],
+		}
 
-    # file { "/tmp/httpd_package" :
-    #     purge  => true,
-    #     recurse => true,
-    #     force => true,
-    #     ensure  => "directory",
-    #     require => Package["httpd"]
-    # }
+		exec { "ssl_conf_backup" :
+		    cwd         => "/etc/httpd/conf.d",
+		    command     => "mv ssl.conf ssl.conf.bkup",
+		    require     => Package["mod_ssl"],
+		}
 
-    # exec { "wget '${config_url}' -O config.jar" :
-    #     alias   => "fetch_httpd_config_package",
-    #     cwd     => "/tmp/httpd_package",
-    #     require => File["/tmp/httpd_package"]
-    # }
-
-    # exec { "jar -xvf config.jar" :
-    #     cwd     => "/tmp/httpd_package",
-    #     alias   => "unjar_httpd_package",
-    #     require => Exec["fetch_httpd_config_package"]
-    # }
-
-    # file { "/tmp/apply_conf_part.sh" :
-    #     source => "puppet:///modules/httpd/apply_conf_part.sh"
-    # }
-
-    # file { "/tmp/httpd.conf.part" :
-    #     source => "puppet:///modules/httpd/httpd.conf.part"
-    # }
-
-    # file { "/tmp/ssl.conf.part" :
-    #     source => "puppet:///modules/httpd/ssl.conf.part"
-    # }
-    
-    ###### httpd.conf ######
-    ## take a backup
-    # file { "/etc/httpd/conf/httpd.conf.bk" :
-    #     source => "/etc/httpd/conf/httpd.conf",
-    #     require => Package["httpd"]
-    # }
-
-    ## create a httpd.conf from default
-    # file { "/etc/httpd/conf/httpd.conf" :
-    #     source => "puppet:///modules/httpd/httpd.conf.default",
-    #     require => File["/etc/httpd/conf/httpd.conf.bk"],
-    #     notify => Service["httpd"]
-    # }
-
-    ## apply httpd.conf.part
-    # exec { "sh /tmp/apply_conf_part.sh /tmp/httpd.conf.part /etc/httpd/conf/httpd.conf" :
-    #     require => File["/tmp/httpd.conf.part", "/etc/httpd/conf/httpd.conf", "/etc/httpd/conf/httpd.conf.bk", "/tmp/apply_conf_part.sh"],
-    #     alias => "apply_httpd_conf_part",
-    #     notify => Service["httpd"]
-    # }
-
-    ###### ssl.conf ######
-    ## take a backup
-    # file { "/etc/httpd/conf.d/ssl.conf.bk" :
-    #     source => "/etc/httpd/conf.d/ssl.conf",
-    #     require => Package["httpd"]
-    # }
-
-    ## create a ssl.conf from default
-    # file { "/etc/httpd/conf.d/ssl.conf" :
-    #     source => "puppet:///modules/httpd/ssl.conf.default",
-    #     require => File["/etc/httpd/conf.d/ssl.conf.bk"],
-    #     notify => Service["httpd"]
-    # }
-
-    ## apply ssl.conf.part
-    # exec { "sh /tmp/apply_conf_part.sh /tmp/ssl.conf.part /etc/httpd/conf.d/ssl.conf" :
-    #     require => File["/tmp/ssl.conf.part", "/etc/httpd/conf.d/ssl.conf", "/etc/httpd/conf.d/ssl.conf.bk", "/tmp/apply_conf_part.sh"],
-    #     alias => "apply_ssl_conf_part",
-    #     notify => Service["httpd"]
-    # }
+		file { "/etc/httpd/conf.d/ssl.conf":
+		   content      => template("httpd/ssl.conf.erb"),
+		   require      => Exec["ssl_conf_backup"],
+	       notify       => Service["httpd"],
+		}
+	}
 }
