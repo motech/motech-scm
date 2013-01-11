fa4e97d60dadc3735d4de6f8a27d0549f2db969c
Anoop Hallur <anooprh@thoughtworks.com>
2013-01-11 11:32:54 +0530

    Anoop,Vishnu | #211 | changing puppet init.pp to rename the downlaoded jasper zip

--- puppet/modules/jasperserver/manifests/init.pp
+++ puppet/modules/jasperserver/manifests/init.pp
@@ -1,7 +1,7 @@
 class jasperserver () {
 
     exec {"get_jasperserver":
-        command     => "/usr/bin/wget /tmp/jasperserver-5.0.0.zip http://nchc.dl.sourceforge.net/project/jasperserver/JasperServer/JasperReports%20Server%205.0.0/jasperreports-server-cp-5.0.0-bin.zip",
+        command     => "/usr/bin/wget -O /tmp/jasperserver-5.0.0.zip http://nchc.dl.sourceforge.net/project/jasperserver/JasperServer/JasperReports%20Server%205.0.0/jasperreports-server-cp-5.0.0-bin.zip",
         timeout     => 0,
         provider    => "shell",
         onlyif      => "test ! -f /tmp/jasperserver-5.0.0.zip"
