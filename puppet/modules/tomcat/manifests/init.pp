6e70d071e924d05976c0a11f4bc1843d2acfc146
f1codz <imdad.ahmed@gmail.com>
2013-01-25 18:36:03 +0530

    Imdad, Vishnu | #00 | Corrected tomcat init.pp and the content in catalina.sh

--- puppet/modules/tomcat/manifests/init.pp
+++ puppet/modules/tomcat/manifests/init.pp
@@ -1,4 +1,4 @@
-class tomcat ( $version, $userName, $tomcatManagerUserName = "tomcat", $tomcatManagerPassword, $tomcatInstance = "", $tomcatHttpPort = "8080", $tomcatRedirectPort = "8443", $tomcatShutdownPort = "8005", $tomcatAjpPort = "8009", $tomcatManagerRoles) {
+class tomcat ( $version, $userName, $tomcatManagerUserName = "tomcat", $tomcatManagerPassword, $tomcatInstance = "", $tomcatHttpPort = "8080", $tomcatRedirectPort = "8443", $tomcatShutdownPort = "8005", $tomcatAjpPort = "8009", $tomcatManagerRoles = "") {
 
     exec {"gettomcattarfile" :
         command     => "/usr/bin/wget -O /tmp/apache-tomcat-${version}.tar.gz http://motechrepo.github.com/pub/motech/other/apache-tomcat-${version}.tar.gz",
@@ -10,7 +10,7 @@ class tomcat ( $version, $userName, $tomcatManagerUserName = "tomcat", $tomcatMa
 
     if $tomcatInstance != "" {
         $instanceSuffix = "-${tomcatInstance}"
-        $moveAfterExtractCommand = "mv apache-tomcat-${version} apache-tomcat-${version}${instanceSuffix}"
+        $moveAfterExtractCommand = "mv apache-tomcat-${version} /home/${userName}/apache-tomcat-${version}${instanceSuffix}"
     } else {
 	    $instanceSuffix = ""
 	    $moveAfterExtractCommand = ""
@@ -21,7 +21,7 @@ class tomcat ( $version, $userName, $tomcatManagerUserName = "tomcat", $tomcatMa
     exec { "tomcat_untar":
         command     => "tar xfz /tmp/apache-tomcat-${version}.tar.gz; $moveAfterExtractCommand",
         user        => "${userName}",
-        cwd         => "/home/${userName}",
+        cwd         => "/tmp/",
         creates     => "$tomcatInstallationDirectory",
         path        => ["/bin"],
         require     => [Exec["$userName homedir"], Exec["gettomcattarfile"]],
@@ -51,6 +51,7 @@ class tomcat ( $version, $userName, $tomcatManagerUserName = "tomcat", $tomcatMa
             content     => template("tomcat/catalina.sh.erb"),
             group       => "${userName}",
             owner       => "${userName}",
+            mode        => 755,
             require     => File["/etc/init.d/tomcat${instanceSuffix}"],
     }
 
