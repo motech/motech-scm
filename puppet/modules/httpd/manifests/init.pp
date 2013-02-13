26e1a685a0011ca4da75633bac5a1bdcdf38540c
Aravind M <arav.psgtech@gmail.com>
2013-02-13 10:58:19 +0530

    Aravind | #00 | Servername parameter.

--- puppet/modules/httpd/manifests/init.pp
+++ puppet/modules/httpd/manifests/init.pp
@@ -1,7 +1,7 @@
 # This class as of now ensures httpd installed and running
 # Project specific rules need to be inserted manually into httpd.conf and ssl.conf
 
-class httpd ($sslEnabled = false, $sslCertificateFile = "/etc/pki/tls/certs/localhost.crt", $sslCertificateKeyFile = "/etc/pki/tls/private/localhost.key", $sslCertificateChainFile = "", $sslCACertificateFile = "") {
+class httpd ($sslEnabled = false, $sslCertificateFile = "/etc/pki/tls/certs/localhost.crt", $sslCertificateKeyFile = "/etc/pki/tls/private/localhost.key", $sslCertificateChainFile = "", $sslCACertificateFile = "", $serverName = "") {
 
     package { "httpd" :
         ensure => "present"
