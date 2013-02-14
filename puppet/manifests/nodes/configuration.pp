4967eeea6696f17bf9d351fc47720a8d0eaa53d8
Hariharan <hariharanweb@gmail.com>
2013-02-14 10:40:23 +0530

    Anoop, Hari | #00 | Added info for patches

--- puppet/manifests/nodes/configuration.pp
+++ puppet/manifests/nodes/configuration.pp
@@ -139,7 +139,7 @@ $jasperDbUsername = "postgres"
 $jasperDbPassword = "password"
 $jasperDbName = "jasperserver"
 $jasperResetDb = "y" ## Provide "y" or "n"
-$jasperPatches = "patch_add_export_restriction"
+$jasperPatches = "patch_add_export_restriction"  #space separated list
 ######################## JASPER CONFIG END################################################
 
 
@@ -156,7 +156,7 @@ $jasperPatches = "patch_add_export_restriction"
 # class { hostname : host_name => "${host_name}" }
 # class { httpd : sslEnabled => $sslEnabled, sslCertificateFile => "${SSLCertificateFile}", sslCertificateKeyFile => "${SSLCertificateKeyFile}", sslCertificateChainFile => $sslCertificateChainFile, sslCACertificateFile => $sslCACertificateFile, serverName => $serverName}
 # class { tomcat : version => "7.0.22", userName => "${motechUser}", tomcatManagerUserName => "${tomcatManagerUserName}", tomcatManagerPassword => "${tomcatManagerPassword}", tomcatInstance => "${tomcatInstance}", tomcatHttpPort => "${tomcatHttpPort}", tomcatRedirectPort => "${tomcatRedirectPort}", tomcatShutdownPort => "${tomcatShutdownPort}", tomcatAjpPort => "${tomcatAjpPort}" }
-# class { jasperserver :, jasperPatches = "${jasperPatches}" }
+# class { jasperserver : jasperPatches => "${jasperPatches}" }
 # class { couchdblucene : version => "0.9.0-SNAPSHOT" }
 # class { repmgr : postgresVersion => "9.1", repmgrVersion => "1.2.0" }
 # class { scripts : urlOfScriptsJar => "your project scripts jar" }
