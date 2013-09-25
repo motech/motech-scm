77e2b0a49b500ec0f94cae1915890d36d299d6eb
Ashish Mishra <ashishkm@thoughtworks.com>
2013-09-25 16:42:28 +0530

    Ashish|#00|Ensuring that hostname is added as first entry in /etc/hosts

--- puppet/modules/hostname/templates/config-hostname.sh
+++ puppet/modules/hostname/templates/config-hostname.sh
@@ -1,5 +1,9 @@
 hostname=$1
-echo "127.0.0.1     $hostname" >> /etc/hosts
-sed -i 's/HOSTNAME.*/HOSTNAME='"$hostname"'/g' /etc/sysconfig/network 
-hostname "$hostname"
 
+echo "127.0.0.1    $hostname $lineIdentifier" > /tmp/hosts
+cat /etc/hosts >> /tmp/hosts
+mv /tmp/hosts /etc/hosts
+
+sed -i 's/HOSTNAME.*/HOSTNAME='"$hostname"'/g' /etc/sysconfig/network
+
+hostname "$hostname"
