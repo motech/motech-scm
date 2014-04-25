7d7656d7ae8cde0dbe36be7a06618d8b1d576e40
sidguptajhs <sidguptajhs@yahoo.in>
2014-04-25 14:13:23 +0530

    Sid | Removing OpenJDK before adding Oracle JDK

--- puppet/modules/java7/manifests/init.pp
+++ puppet/modules/java7/manifests/init.pp
@@ -1,12 +1,22 @@
 class java7($motechUser,$arch="x64"){
 
+
+    package { "java-1.7.0-openjdk.x86_64":
+        ensure => absent,
+    }
+
+    package { "java-1.6.0-openjdk.x86_64":
+        ensure => absent,
+    }
+
+
     exec { "downloadOracleJava7" :
       command     => "/usr/bin/wget -O /tmp/java/jdk-7u51-linux-${arch}.rpm --no-check-certificate --no-cookies  --header 'Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-${arch}.rpm",
       user        => "${motechUser}",
       timeout     => 0,
       provider    => "shell",
       onlyif      => "test ! -f /tmp/java/jdk-7u51-linux-${arch}.rpm",
-      require     => File["/tmp/java/"]
+      require     => [File["/tmp/java/"], Package["java-1.7.0-openjdk.x86_64"], Package["java-1.6.0-openjdk.x86_64"]]
     }
 
     exec {"installJava7" :
